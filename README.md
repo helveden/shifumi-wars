54686ab1e856891ae7f5dd3d8b553c77f1a14be2
Helveden <acharrier@iceb.com>
2022-06-30 15:42:02 +0200

    Readme Translation, React HTML - Tableaux des dernières partie créée

--- README.md
+++ README.md
@@ -7,3 +7,8 @@ Page d'accueil Principale
 Non connecté
 
 Connecté
+
+@Todos
+# Translations
+
+https://medium.com/swlh/how-we-translated-a-symfony-and-react-project-67906b7d5b30
--- assets/components/AllGames.js
+++ assets/components/AllGames.js
@@ -29,6 +29,35 @@ class AllGames extends Component {
         return (
             <>
                 <p>All games</p>
+                <nav>
+                    <ul class="d-flex">
+                        <li><button>Create Game</button></li>
+                        <li><button>Rejoindre une Game</button></li>
+                    </ul>
+                </nav>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Nom</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Classic - Well</td>
+                            <td>
+                                <ul class="d-flex">
+                                    <li>
+                                        <button>Show</button>
+                                    </li>
+                                    <li>
+                                        <button>Rejoindre</button>
+                                    </li>
+                                </ul>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
             </>
         ) 
     }
