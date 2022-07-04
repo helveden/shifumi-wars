ce4ffed11427f8d2e6294c8fdd6b80074d6687a8
Helveden <charrieralexis0@gmail.com>
2022-07-04 23:43:24 +0200

    Popin game

--- assets/components/AllGames.js
+++ assets/components/AllGames.js
@@ -13,13 +13,22 @@ class AllGames extends Component {
         super(props);
         
         this.state = {
-            show: false,
+            openGame: false,
+            showCreateGame: false,
             datas: [],
+            rounds: [],
             filters: {}
         };
 
-        this.handleShow = this.handleShow.bind(this);
-        this.handleClose = this.handleClose.bind(this);
+        this.handleShowCreateGame = this.handleShowCreateGame.bind(this);
+        this.handleCloseCreateGame = this.handleCloseCreateGame.bind(this);
+        
+        
+        this.handleOpenGame = this.handleOpenGame.bind(this);
+        this.handleCloseGame = this.handleCloseGame.bind(this);
+
+        
+        this.addRound = this.addRound.bind(this);
         
         // this.handleChange = this.handleChange.bind(this);
         // this.handleSubmit = this.handleSubmit.bind(this);
@@ -37,69 +46,122 @@ class AllGames extends Component {
         }
     }
 
-    handleClose(event) {
+    handleCloseCreateGame(event) {
         this.setState({
-            show: false
+            showCreateGame: false
         });
     }
 
-    handleShow(event) {
+    handleShowCreateGame(event) {
         this.setState({
-            show: true
+            showCreateGame: true
+        });
+    } 
+
+    handleOpenGame(event) {
+        this.setState({
+            openGame: true
+        });
+    }
+
+    handleCloseGame(event) {
+        this.setState({
+            openGame: false
+        });
+    }
+
+    addRound(select) {
+        this.state.rounds.push({msg: select});
+
+        let rounds = this.state.rounds
+        this.setState({
+            rounds: rounds
         });
     }
     
     render() {
         const { t } = this.props;
-        const show = this.state.show  
+        const showCreateGame = this.state.showCreateGame  
+        const openGame = this.state.openGame
+        const rounds = this.state.rounds
         
         return (
             <>
             
                 <p>{t('allgames.title')}</p>
                 <nav>
-                    <ul className="d-flex">
-                        <li><button onClick={this.handleShow}>{t('game.create')}</button></li>
+                    <ul className='d-flex'>
+                        <li><button onClick={this.handleShowCreateGame}>{t('game.create')}</button></li>
                         <li><button>{t('allgames.nav.buttons.find_game')}</button></li>
                     </ul>
                 </nav>
-                <table className="table">
-                    <thead>
-                        <tr>
-                            <th>{t('allgames.table.head.name')}</th>
-                            <th>{t('allgames.table.head.config')}</th>
-                            <th>{t('allgames.table.head.players')}</th>
-                            <th>{t('allgames.table.head.actions')}</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>Nom du jeu</td>
-                            <td>
-                                <ul className="d-flex align-items-center game-name-list">
-                                    <li>Classic - Tournoi - Well - No password</li>
-                                    <li><Icon icon="paper"/></li>
-                                    <li><Icon icon="rock"/></li>
-                                    <li><Icon icon="scissors"/></li>
-                                    <li><Icon icon="spock"/></li>
-                                    <li><Icon icon="lizard"/></li>
-                                </ul>
-                            </td>
-                            <td><span className="d-flex align-items-center">2 / 8&nbsp;<Icon icon="usergroup"/></span></td>
-                            <td>
-                                <ul className="d-flex">
-                                    <li>
-                                        <button>{t('allgames.table.actions.show')}</button>
-                                    </li>
-                                    <li>
-                                        <button>{t('allgames.table.actions.meet')}</button>
-                                    </li>
-                                </ul>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-                <Modal show={show} onHide={this.handleClose}>
+                { openGame ? 
+                    <div id='game'>
+                        <div className=''>
+                            <section className='wrap-game'>
+                                <header className='game-header '>
+                                    <nav>
+                                        <ul className='game-nav d-flex justify-content-end align-items-center'>
+                                            <li><button onClick={this.handleCloseGame}><Icon icon='close'/></button></li>
+                                        </ul>
+                                    </nav>
+                                </header>
+                                <div className='game-content'>
+                                    <ul>                                        
+                                        {rounds.map((round, index) => {
+                                            return <li key={index} className="round">{round.msg}</li>
+                                        })}
+                                    </ul>
+                                </div>
+                                <footer>
+                                    <ul className='game-actions d-flex justify-content-center align-items-center'>
+                                        <li><button onClick={() => {this.addRound('paper')}}><Icon icon='paper'/></button></li>
+                                        <li><button onClick={() => {this.addRound('rock')}}><Icon icon='rock'/></button></li>
+                                        <li><button onClick={() => {this.addRound('scissors')}}><Icon icon='scissors'/></button></li>
+                                    </ul>
+                                </footer>
+                            </section>
+                        </div>
+                    </div>
+                    :                    
+                    <table className='table'>
+                        <thead>
+                            <tr>
+                                <th>{t('allgames.table.head.name')}</th>
+                                <th>{t('allgames.table.head.config')}</th>
+                                <th>{t('allgames.table.head.players')}</th>
+                                <th>{t('allgames.table.head.actions')}</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Nom du jeu</td>
+                                <td>
+                                    <ul className='d-flex align-items-center game-name-list'>
+                                        <li>Classic - Tournoi - Well - No password</li>
+                                        <li><Icon icon='paper'/></li>
+                                        <li><Icon icon='rock'/></li>
+                                        <li><Icon icon='scissors'/></li>
+                                        <li><Icon icon='spock'/></li>
+                                        <li><Icon icon='lizard'/></li>
+                                    </ul>
+                                </td>
+                                <td><span className='d-flex align-items-center'>2 / 8&nbsp;<Icon icon='usergroup'/></span></td>
+                                <td>
+                                    <ul className='d-flex'>
+                                        <li>
+                                            <button onClick={this.handleOpenGame}>{t('allgames.table.actions.show')}</button>
+                                        </li>
+                                        <li>
+                                            <button>{t('allgames.table.actions.meet')}</button>
+                                        </li>
+                                    </ul>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                }
+                <Modal show={showCreateGame} onHide={this.handleCloseCreateGame}>
                     <Modal.Header closeButton>
                         <Modal.Title>{t('game.create')}</Modal.Title>
                     </Modal.Header>
--- assets/components/partials/Icon.js
+++ assets/components/partials/Icon.js
@@ -8,6 +8,8 @@ import HandScissors from '../../pictures/svg/hand-scissors-solid.svg';
 import HandSpock from '../../pictures/svg/hand-spock-solid.svg';
 import User from '../../pictures/svg/user-solid.svg';
 import UserGroup from '../../pictures/svg/user-group-solid.svg';
+import CloseFill from '../../pictures/svg/x-circle-fill.svg';
+import Close from '../../pictures/svg/x.svg';
 
 class Icon extends Component {
     
@@ -20,12 +22,14 @@ class Icon extends Component {
 
         this.state.allSvg = {}
         this.state.allSvg['paper'] = HandPaper,
-        this.state.allSvg['rock']  = HandRock,
-        this.state.allSvg['lizard']  = HandLizard,
-        this.state.allSvg['scissors']  = HandScissors,
-        this.state.allSvg['spock']  = HandSpock
-        this.state.allSvg['user']  = User
-        this.state.allSvg['usergroup']  = UserGroup
+        this.state.allSvg['rock'] = HandRock,
+        this.state.allSvg['lizard'] = HandLizard,
+        this.state.allSvg['scissors'] = HandScissors,
+        this.state.allSvg['spock'] = HandSpock
+        this.state.allSvg['user'] = User
+        this.state.allSvg['usergroup'] = UserGroup
+        this.state.allSvg['closefill'] = CloseFill
+        this.state.allSvg['close'] = Close
     }
     
     componentDidMount() {
